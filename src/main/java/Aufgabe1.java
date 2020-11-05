7aab9db7f0b1f8c0e77b93ced8af3960cf3b3204
elias kupeczki <eliaskupeczki@gmail.com>
2020-11-05 20:14:35 +0100

    Project setup

--- src/main/java/Aufgabe1.java
+++ src/main/java/Aufgabe1.java
@@ -1,8 +1,10 @@
+import princeton.stdlib.StdIn;
+
 public class Aufgabe1 {
     public static void main(String[] args) {
         System.out.println("Enter an Integer number:");
-        
-        if ( 20 % 2 == 0 )
+        StdIn:
+        if ( StdIn % 2 == 0 )
             System.out.println("Entered number is even");
         else
             System.out.println("Entered number is odd");
--- src/main/java/Aufgabe2.java
+++ src/main/java/Aufgabe2.java
@@ -1,5 +1,6 @@
 public class Aufgabe2 {
     public static void main(String[] args) {
+    System.out.println("Enter the Number of Rows: 5");
 
     }
 }
