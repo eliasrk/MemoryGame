f8dfce89f17f4238384a5e458c74fe5a6a0223cc
elias kupeczki <eliaskupeczki@gmail.com>
2020-11-07 11:41:18 +0100

    Project setup

---
+++ src/main/java/Pascals_Triangle.java
@@ -0,0 +1,25 @@
+import princeton.stdlib.StdIn;
+
+public class Pascals_Triangle {
+    public static void main(String[] args) {
+        int num;
+        int j;
+        System.out.println("number of rows?");
+        num = StdIn.readInt();
+        int Middle;
+        Middle = 1;
+        for (int i = 0; i < num; i++) //depth of rows
+             {
+            for(j=0; j <= i; j++)//amount of numbers per row
+            {
+                if (j==0 || i==0)
+                    Middle = 1;//if row is 1 or begining or ending of a row equals 1
+                else
+                    Middle = Middle*(i-j+1)/j;//
+
+                System.out.print(Middle + " ");
+            }
+            System.out.println("\n");
+        }
+    }
+}
--- src/main/java/Pythagoras.java
+++ src/main/java/Pythagoras.java
@@ -1,37 +1,46 @@
 import princeton.stdlib.StdIn;
 
+
 public class Pythagoras {
+    public static void Method1() {
+        System.out.println("what length is A?");
+        int A = StdIn.readInt ();
+        System.out.println("what length is B?");
+        int B = StdIn.readInt ();
+        double C = Math.pow(A,2) + Math.pow(B,2);
+        C= Math.sqrt(C);
+        System.out.println(C);
+    }public static void Method2() {
+        System.out.println("what length is B?");
+        int B = StdIn.readInt ();
+        System.out.println("what length is C?");
+        int C = StdIn.readInt ();
+        double A = Math.pow(C,2)- Math.pow(B,2);
+        A= Math.sqrt(A);
+        System.out.println(A);
+    }
+    public static void Method3(){
+        System.out.println("what length is C?");
+        int C = StdIn.readInt ();
+        System.out.println("what length is A?");
+        int A = StdIn.readInt ();
+        double B = Math.pow(C,2)- Math.pow(A,2);
+        B= Math.sqrt(B);
+        System.out.println(B);
+    }
     public static void main(String[] args) {
         int sides;
         System.out.println("which side don´t you have ?");
         System.out.println("1 = C 2 = A 3 = B");
         sides = StdIn.readInt ();
         if (sides == 1) {
-            System.out.println("what length is A?");
-            int A = StdIn.readInt ();
-            System.out.println("what length is B?");
-            int B = StdIn.readInt ();
-            double C = Math.pow(A,2) + Math.pow(B,2);
-            C= Math.sqrt(C);
-            System.out.println(C);
+            Method1();
         }
         if (sides == 2) {
-            System.out.println("what length is B?");
-            int B = StdIn.readInt ();
-            System.out.println("what length is C?");
-            int C = StdIn.readInt ();
-            double A = Math.pow(C,2)- Math.pow(B,2);
-            A= Math.sqrt(A);
-            System.out.println(A);
+            Method2();
         }
         if (sides == 3) {
-            System.out.println("what length is C?");
-            int C = StdIn.readInt ();
-            System.out.println("what length is A?");
-            int A = StdIn.readInt ();
-            double B = Math.pow(C,2)- Math.pow(A,2);
-            B= Math.sqrt(B);
-            System.out.println(B);
+            Method3();
         }
     }
 }
