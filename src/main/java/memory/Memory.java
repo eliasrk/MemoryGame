53dab8fea37ddee61e26b7e6a9bfee0e5318c0af
elias kupeczki <eliaskupeczki@gmail.com>
2020-11-14 08:32:52 +0100

    Project setup

--- src/main/java/memory/Memory.java
+++ src/main/java/memory/Memory.java
@@ -1,6 +1,22 @@
 package memory;
 
 public class Memory {
+
+    public static void Nothing() {
+        String[][] gameBoard = {{"  ", "1", " 2", " 3", " 4"},
+                {"1 ", "?", " ?", " ?", " ?"},
+                {"2 " + "? " + "? " + "? " + "?"},
+                {"3 " + "? " + "? " + "? " + "?"},
+                {"2 " + "? " + "? " + "? " + "?"}};
+        for (String[] row : gameBoard) {
+            for (String c : row) {
+                System.out.print(c);
+            }
+            System.out.println();
+        }
+
+
+    }
     public static void main(String[] args) {
 
     }
